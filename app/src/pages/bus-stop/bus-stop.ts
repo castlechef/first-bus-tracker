8846e6d546484a88235fd8c81da5ebb44128bb8e
Saixos <xsaixosx@gmail.com>
2018-03-15 15:29:13 +0000

    Improved bus stop modal & added color
    Added list of incoming buses to modal
    
    made everything pink

--- app/src/pages/bus-stop/bus-stop.ts
+++ app/src/pages/bus-stop/bus-stop.ts
@@ -17,8 +17,14 @@ export class BusStopPage {
 
   public title = "BusStop";
 
+  buses: Array<{ busRoute: string, arrivalTime: string }>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewctrl: ViewController) {
     this.title = navParams.get('stopName');
+    this.buses = [
+      { busRoute: 'U1', arrivalTime: "09:50" },
+      { busRoute: 'U1X', arrivalTime: "09:53" }
+    ];
   }
 
   ionViewDidLoad() {
@@ -28,6 +34,35 @@ export class BusStopPage {
   closeModal(){
     this.viewctrl.dismiss();
   }
+
+  /*"busStopId": 1,
+                "busStopName": "Junction Road",
+                "location": {
+                    "latitude": 52.3456546,
+                    "longitude": -1.3465544
+                },
+                "busRoutePosition": [
+                    {
+                        "name": "U1X",
+                        "position": 4
+                    },
+                    {
+                        "name": "U1",
+                        "position": 7
+                    }
+                ],
+                "arrivals": [
+                    {
+                        "busId": 1,
+                        "routeName": "U1",
+                        "arrivalTime": "09:50"
+                    },
+                    {
+                        "busId": 2,
+                        "routeName": "U1X",
+                        "arrivalTime": "09:53"
+                    }
+                ]*/
 }
 
 
