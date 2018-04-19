092afe91c84db0249307a77b9d8d8881335884da
= <beaumont_jonny@hotmail.co.uk>
2018-04-19 21:29:12 +0100

    Fixed hostname

--- app/src/app/main.js
+++ app/src/app/main.js
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-export var HOST = 'localhost/api';
+export var HOST = 'http://localhost:8080/api';
 platformBrowserDynamic().bootstrapModule(AppModule);
 //# sourceMappingURL=main.js.map
--- app/src/app/main.ts
+++ app/src/app/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-export const HOST: string = 'localhost/api';
+export const HOST: string = 'http://localhost:8080/api';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
